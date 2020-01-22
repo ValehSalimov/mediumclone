ca745d4d2d1a184b75bc6e9e4b5f9c711793fe18
Valeh Salimov <valehsalimov.vs@gmail.com>
2020-01-22 18:12:03 +1100

    Пагинация

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -5,7 +5,7 @@ import { CurrentUserContext } from '../contexts/currentUser';
 
 const TopBar = () => {
   const [currentUserState] = useContext(CurrentUserContext);
-  console.log(currentUserState)
+  
   return (
     <nav className="navbar navbar-light">
       <div className="container">
